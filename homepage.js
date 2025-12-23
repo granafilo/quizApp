5950e853bb9575b5300149a6f0e739978463e1ba
granafilo <granafilo@gmail.com>
2025-12-23 12:46:30 +0100

    spostamento file

---
+++ homepage.js
@@ -0,0 +1,104 @@
+import { loadFroamStorage, saveToStorage } from "./storage/storageFunctions.js";
+
+let nickname = document.getElementById('nickname');
+nickname.value = '';
+
+//aggiungo event listener per iniziare il gioco
+const startGameBtn = document.getElementById('startGameBtn');
+
+startGameBtn.addEventListener('click', () => {
+    startGame();
+})
+
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+        startGame();
+    }
+});
+
+const scoreBoard = loadFroamStorage() || [];
+const scoreBoardHtml = document.getElementById('scoreBoard');
+let scoreHtml = '';
+
+scoreBoard.forEach((tentativo) => {
+    const nickname = tentativo.username;
+    const score = tentativo.score;
+
+    scoreHtml = `
+            <div class="scoreBoardHeader p-2" style="justify-content: start !important; background: white !important; color: black !important; border: 1px solid #e9ecef !important; ">
+                <span style="font-weight: 900; font-size: 30px;"
+                    class="text-wrap text-break">${nickname}</span>
+            </div>
+
+            <div class="scoreBoardHeader" style="background: white !important; color: black !important; border: 1px solid #e9ecef !important; ">
+                <span style="font-weight: 800; font-size: 30px;"
+                    class="text-wrap text-break">${score.punteggio}</span>
+            </div>
+
+            <div class="scoreBoardHeader" style="background: white !important; color: black !important; border: 1px solid #e9ecef !important; ">
+                <span style="font-weight: 800; font-size: 20px;"
+                    class="text-break text-wrap">${score.corrette}</span>
+            </div>
+
+            <div class="scoreBoardHeader" style="background: white !important; color: black !important; border: 1px solid #e9ecef !important; ">
+                <span style="font-weight: 800; font-size: 20px;"
+                    class="text-break text-wrap">${score.errate}</span>
+            </div>
+    `;
+
+    scoreBoardHtml.innerHTML += scoreHtml;
+})
+
+const choosePfpBtn = document.getElementById('choosePfpBtn');
+const pfpContainer = document.getElementById('pfpContainer');
+const currentPfp = document.getElementById('currentPfp');
+const difficultySelected = document.getElementById('difficultySelected');
+let pfpShown = false;
+
+choosePfpBtn.addEventListener('click', () => {
+    if (!pfpShown) {
+        pfpContainer.classList.remove('d-none');
+        difficultySelected.disabled = true;
+        pfpShown = true;
+    } else {
+        pfpContainer.classList.add('d-none');
+        difficultySelected.disabled = false;
+        pfpShown = false;
+    }
+})
+
+document.querySelectorAll('.js-pfp').forEach((btn) => (
+    btn.addEventListener(('click'), () => {
+        pfpShown = false;
+        difficultySelected.disabled = false;
+        const srcNewPfp = btn.src;
+        currentPfp.src = srcNewPfp;
+        currentPfp.name = btn.id;
+        pfpContainer.classList.add('d-none');
+    })
+));
+
+document.querySelectorAll('.dropdown-item').forEach(item => {
+    item.addEventListener('click', function () {
+        document.querySelector('.dropdown-toggle').textContent = this.textContent;
+    });
+});
+
+nickname.addEventListener('click', () => {
+    if (pfpShown) {
+        pfpContainer.classList.add('d-none');
+        difficultySelected.disabled = false;
+        pfpShown = false;
+    }
+});
+
+function startGame() {
+    const nickname = document.getElementById('nickname').value;
+    const difficulty = document.getElementById('difficultySelected').textContent.toLowerCase();
+    const pfp = currentPfp.name;
+    if (nickname.trim() === '') {
+        alert('Please enter a nickname to start the game.');
+        return;
+    }
+    window.location.href = `game.html?nickname=${nickname}&difficulty=${difficulty}&pfpId=${pfp}`
+}
