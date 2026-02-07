38d978ced09d2a9c0ed656cde13d883116c6c53e
granafilo <granafilo@gmail.com>
2026-02-07 16:06:36 +0100

    modificato url vite

--- quizapp/vite.config.js
+++ quizapp/vite.config.js
@@ -3,5 +3,5 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   // Sostituisci 'NOME-REPO' con il nome della tua cartella su GitHub
   // Esempio: se il tuo sito è su github.com/mario/il-mio-sito, scriverai '/il-mio-sito/'
-  base: '/quizzApp/', 
+  base: '/', 
 })
